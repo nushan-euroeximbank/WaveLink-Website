e463e511293a2f64cb61543c18b126b15f75c4d7
nushankodikara <nushankodi@gmail.com>
2024-02-01 19:50:53 +0530

    refreshed project

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit()],
-
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData: '@use "src/variables.scss" as *;'
-			}
-		}
-	}
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()]
+});
